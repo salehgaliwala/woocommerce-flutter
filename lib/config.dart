da3c3b3abcdd865d2173c0b381a634645f682f4e
salehgaliwala <unimax.systems@gmail.com>
2021-11-13 11:57:27 +0530

    updating configs

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,9 @@
 class Config {
-  static String key = "ck_eacab4e81d56d8776408a0030302375779864007";
-  static String secret = "cs_dad5772f26b1a170bea51ee8f64f57ff5583b86d";
-  static String url = "https://brasabeer.com/wp-json/wc/v3/";
+  static String key = "xxx";
+  static String secret = "xxx";
+  static String url = "xxx";
   static String customerURL = "customers";
-  static String tokenURL = "https://brasabeer.com/wp-json/jwt-auth/v1/token";
+  static String tokenURL = "https://yourdomainname.com/wp-json/jwt-auth/v1/token";
   static String categoriesURL = "products/categories";
   static String productsURL = "products";
   static String todayOffersTagId = "42";
